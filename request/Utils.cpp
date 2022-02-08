d25069add8631ec5923087a13a75c6622c4f5b36
mohamedamine456 <aminelachheb456@gmail.com>
2022-02-08 19:07:09 +0100

    not a propre way

--- request/Utils.cpp
+++ request/Utils.cpp
@@ -1,16 +1,29 @@
 #include "Utils.hpp"
 
-std::vector<std::string>    ft_split( std::string str, std::string delimiter ) {
-    std::vector<std::string> parts;
-    while (str.length() && str.find(delimiter) != std::string::npos) {
-        std::string tmp = str.substr(0, str.find(delimiter));
-        if (tmp.length() > 0) {
-            parts.push_back(tmp);
-        }
-        str.erase(0, str.find(delimiter) + 1);
-    }
-    if (str.length() > 0) {
-        parts.push_back(str);
-    }
-    return parts;
+std::vector<std::string>    StringSplit( std::string str, std::string delimiter ) {
+	std::vector<std::string> parts;
+	while (str.length() && str.find(delimiter) != std::string::npos) {
+		std::string tmp = str.substr(0, str.find(delimiter));
+		if (tmp.length() > 0) {
+			parts.push_back(tmp);
+		}
+		str.erase(0, str.find(delimiter) + 1);
+	}
+	if (str.length() > 0) {
+		parts.push_back(str);
+	}
+	return parts;
+}
+
+
+bool                        checkHeaders( std::string header ) {
+	header = header.substr(0, header.find(": "));
+	std::vector< std::string > baseHeaders = StringSplit(HEADERS, ",");
+
+	for (std::vector< std::string >::iterator it = baseHeaders.begin(); it != baseHeaders.end(); it++) {
+		if (header == *it) {
+			return true;
+		}
+	}
+	return false;
 }
