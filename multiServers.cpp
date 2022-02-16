f4dd37216f67eecbd0bcf8c8eaa300ec53b2bd62
mohamedamine456 <aminelachheb456@gmail.com>
2022-02-16 23:43:24 +0100

    last changes

--- multiServers.cpp
+++ multiServers.cpp
@@ -85,20 +85,20 @@ int main () {
 		exit(EXIT_FAILURE);
 	}
 
-	// sockfd2 = socket(AF_INET, SOCK_STREAM, 0);
-	// if (sockfd2 <= 0) {
-	// 	std::cerr << "Socket Creation Failed!" << std::endl;
-	// 	exit(EXIT_FAILURE);
-	// }
+	sockfd2 = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd2 <= 0) {
+		std::cerr << "Socket Creation Failed!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
 	// initialize address
 	initialize_address(address, 8080);
-	// initialize_address(address2, 8000);
+	initialize_address(address2, 8000);
 
-	if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-		std::cerr << "Bind Failed!" << std::endl;
-		exit(EXIT_FAILURE);
-	}
+	// if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+	// 	std::cerr << "Bind Failed!" << std::endl;
+	// 	exit(EXIT_FAILURE);
+	// }
 
 	// if (bind(sockfd2, (struct sockaddr *)&address2, sizeof(address2)) == -1) {
 	// 	std::cerr << "Bind Failed!" << std::endl;
@@ -110,39 +110,58 @@ int main () {
 		std::cerr << "Listen Failed!" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	// if (listen(sockfd2, 100) < 0) {
-	// 	std::cerr << "Listen Failed!" << std::endl;
-	// 	exit(EXIT_FAILURE);
-	// }
+	if (listen(sockfd2, 100) < 0) {
+		std::cerr << "Listen Failed!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
-	fd_set	current, backup;
-	FD_ZERO(&current);
-	FD_SET(sockfd, &current);
+	// fd_set	current, backup;
+	// FD_ZERO(&current);
+	// FD_SET(sockfd, &current);
 	// FD_SET(sockfd2, &current);
 
 	while (true) {
-		if (select(FD_SETSIZE, &current, NULL, NULL, NULL) < 0) {
-			std::cerr << "Select Failed!" << std::endl;
+		// if (select(FD_SETSIZE, &current, NULL, NULL, NULL) < 0) {
+		// 	std::cerr << "Select Failed!" << std::endl;
+		// 	exit(EXIT_FAILURE);
+		// }
+
+		socklen_t stor_size = sizeof(struct sockaddr_in);
+		int newSockfd = accept(sockfd, (struct sockaddr *)&connAddress, &stor_size);
+		if (newSockfd < 0) {
+			std::cerr << "Accepting Connection Failed!" << std::endl;
 			exit(EXIT_FAILURE);
 		}
-
-		for (int i = 0; i < FD_SETSIZE; i++) {
-			if (FD_ISSET(i, &current)) {
-				if (i == sockfd) {
-					socklen_t stor_size = sizeof(struct sockaddr_in);
-					int newSockfd = accept(i, (struct sockaddr *)&connAddress, &stor_size);
-					if (newSockfd < 0) {
-						std::cerr << "Accepting Connection Failed!" << std::endl;
-						exit(EXIT_FAILURE);
-					}
-					FD_SET(newSockfd, &current);
-				}
-				else {
-					Request rqst = read_request(i);
-					send_simple_response(i);
-					FD_CLR(i, &current);
-				}
-			}
+		else {
+			send_simple_response(newSockfd);
 		}
+
+		int newSockfd2 = accept(sockfd2, (struct sockaddr *)&connAddress2, &stor_size);
+		if (newSockfd2 < 0) {
+			std::cerr << "Accepting Connection Failed!" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		else {
+			send_simple_response(newSockfd2);
+		}
+
+		// for (int i = 0; i < FD_SETSIZE; i++) {
+		// 	if (FD_ISSET(i, &current)) {
+		// 		if (i == sockfd) {
+		// 			socklen_t stor_size = sizeof(struct sockaddr_in);
+		// 			int newSockfd = accept(i, (struct sockaddr *)&connAddress, &stor_size);
+		// 			if (newSockfd < 0) {
+		// 				std::cerr << "Accepting Connection Failed!" << std::endl;
+		// 				exit(EXIT_FAILURE);
+		// 			}
+		// 			FD_SET(newSockfd, &current);
+		// 		}
+		// 		else {
+		// 			Request rqst = read_request(i);
+		// 			send_simple_response(i);
+		// 			FD_CLR(i, &current);
+		// 		}
+		// 	}
+		// }
 	}
 }
