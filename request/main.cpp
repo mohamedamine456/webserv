57823f9d98c954d2d4d63cb38f5f14a3668d05e8
mohamedamine456 <aminelachheb456@gmail.com>
2022-02-18 15:15:28 +0100

    read till headers than check headers for content-length

--- request/main.cpp
+++ request/main.cpp
@@ -1,16 +1,19 @@
 #include "Request.hpp"
 #include "Utils.hpp"
-#include <sys/socket.h>
-#define RECV_SIZE 4096
+#include "../servers/Socket.hpp"
 
-std::string getfilename(std::string str) {
-	static int a = 1;
-	time_t ttime = std::time(0);
-	std::string filename(std::to_string(ttime));
-	filename.insert(filename.length(), std::to_string(a));
-	a++;
-	return (filename);
-}
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <fstream>
+#include <cstdio>
+#include <fcntl.h>
+#define RECV_SIZE 4096
 
 std::string	read_request(int newSockfd) {
 	int					recvLength = RECV_SIZE;
@@ -26,39 +29,60 @@ std::string	read_request(int newSockfd) {
 	return (filename);
 }
 
+void	send_simple_response(int &newSockfd)
+{
+	std::string str_send = "HTTP/1.1 200 OK\nServer: Test Server\nContent-Type: text/plain\nContent-Length: 7\n\nHello!\n";
+	send(newSockfd, str_send.c_str(), strlen(str_send.c_str()), 0);
+}
+
+void	handle_request(int newSockfd)
+{
+	std::string rqst = read_request(newSockfd);						// read request
+	send_simple_response(newSockfd);							// to prevent multi request from mozilla
+	std::cout << "End Reading!" << std::endl;
+	close(newSockfd);
+}
+
 int main() {
-	Request rqst;
-	int							newSockfd = 3;		// should be connection socket fd
-	std::vector< std::string >	parts;
-	std::string					rqstFilename;
-	std::string					bodyFilename;
-	std::ofstream				bodyFile;
-	std::ifstream				requestFile;
-	std::string					rqstLine;
+	std::vector<Socket>	servers = create_multiple_servers();
+	int					sockfd;											// server socket FD
+	int					newSockfd;										// new connection FD										// server configuration
+	struct sockaddr_in	connAddress;
+	socklen_t stor_size = sizeof(struct sockaddr_in);
 
-	rqstFilename = read_request(newSockfd);
-	requestFile.open(rqstFilename, std::ifstream::in);
-	if (requestFile.fail()) {
-		std::cerr << "Open File Failed!" << std::endl;
-		exit(EXIT_FAILURE);
+	fd_set	rfds, rset;
+	int maxfd = -1, fd = -1;
+	unsigned int i, status;
+	FD_ZERO(&rfds);
+	for (std::vector<Socket>::iterator it = servers.begin(); it != servers.end(); it++) {
+		FD_SET((*it).getSocketFd(), &rfds);
+		if ((*it).getSocketFd() > maxfd)
+			maxfd = (*it).getSocketFd();
 	}
-	getline(requestFile, rqstLine);
-	if (rqstLine.empty()) {
-		std::cerr << "Bad Request!" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-
-	while (getline(requestFile, rqstLine)) {
-		if (rqstLine == "") {
-			break ;
+	while (true) {
+		rset = rfds;
+		status = select(maxfd + 1, &rset, NULL, NULL, NULL);
+		if (status < 0) {
+			std::cerr << "Select Failed!" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		for (i = 0; i < servers.size(); i++) {
+			if (FD_ISSET(servers[i].getSocketFd(), &rset)) {
+				fd = servers[i].getSocketFd();
+				break ;
+			}
+		}
+		if (fd == -1) {
+			std::cerr << "No Connection Failed!" << std::endl;
+			exit(EXIT_FAILURE);
 		}
 		else {
-			rqst.addHeader(rqstLine);
+			newSockfd = accept(fd, (struct sockaddr *)&connAddress, &stor_size);
+			if (newSockfd < 0) {
+				std::cerr << "Accepting Connection Failed!" << std::endl;
+				exit(EXIT_FAILURE);
+			}
+			handle_request(newSockfd);
 		}
 	}
-	while (getline(requestFile, rqstLine)) {
-		bodyFile << rqstLine << std::endl;
-	}
-	rqst.setBodyfile("");
-	return (0);
 }
