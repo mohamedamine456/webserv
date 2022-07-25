acfccdc7247b8a91e5b896eb1c0d1dad1f277d25
mohamedamine456 <aminelachheb456@gmail.com>
2022-07-25 18:51:40 +0100

    python working

---
+++ var/www/cgi_tests/welcome_py.py
@@ -0,0 +1,20 @@
+#import cgi module:
+import cgi
+ 
+#output http header:
+print ('Content-type: text/html')
+print ('')
+# note the empty print above is required!
+ 
+print ('<html><head><title>Python Test</title></head>')
+print ('<body>')
+ 
+theRequest = cgi.FieldStorage()
+name = theRequest.getfirst("name", "undefined")
+print ('<h3>First Test OF Python CGI</h3>')
+print("Name of the user is " + name)
+sport = theRequest.getfirst("sport", "undefined")
+print(" plays " + sport + " as Sport")
+year = theRequest.getfirst("year", "undefined")
+print("since " + year + "!")
+print ('</body></html>')
